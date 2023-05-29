84c2160887b803aa919b6679d36195d28e00b4a9
Graigor Pierre-Noel <gpnoel111@yahoo.com>
2023-05-29 15:43:14 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by grogiar _ on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
